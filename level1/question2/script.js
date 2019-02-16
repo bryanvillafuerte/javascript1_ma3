365d3b982f13c2684ec5156786d8b7f79e43c112
bryjho <bryjho.villafuerte@yahoo.com.ph>
2019-02-17 00:11:51 +0100

    commit02

--- level1/question2/script.js
+++ level1/question2/script.js
@@ -1,1 +1,29 @@
-//Console.log out elements in JSON file
+/*Console.log out elements in JSON file
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "author.json", false);
+    xhttp.send();
+
+    var jsonData = JSON.parse(xhttp.responseText);
+    for (var i = 0; i < jsonData.length; i++) {
+        console.log(jsonData[i]);
+    }
+}
+
+loadDoc();*/
+
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var jsonData = JSON.parse(this.responseText);
+            for (var i = 0; i < jsonData.length; i++) {
+                console.log(jsonData[i]);
+            }
+        }
+    };
+    xhttp.open("GET", "author.json", true);
+    xhttp.send();
+}
+
+loadDoc();
