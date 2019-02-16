4c0ebe42c805e47bc48620e80fcef5df423e0b94
bryjho <bryjho.villafuerte@yahoo.com.ph>
2019-02-16 22:32:22 +0100

    commit01

--- level1/question1/script.js
+++ level1/question1/script.js
@@ -1,1 +1,10 @@
-//Use RegEx to validate form
+document.getElementById("submit").addEventListener("click",function() {
+    var inputTest = document.getElementsByTagName("input");
+
+    for(var i=0; i < inputTest.length; i++){
+        var myInput = inputTest[i].value;
+        var myPattern = new RegExp(inputTest[i].pattern);
+        var myTest = myPattern.test(myInput);
+        console.log(myTest);
+    }
+})
